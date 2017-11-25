1a41cc44b6af2cd7a6c6be164d1479db0ff8cc94
yujinhua <184040972@qq.com>
2017-11-25 22:21:56 +0800

    完成header,footer,main,sidebar,滑动侧边栏

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { MatSidenavModule } from '@angular/material';
+import { CoreModule } from './core/core.module';// 导入核心模块，在其他地方就不加载了
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +11,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,32 @@
-import { NgModule } from '@angular/core';
+//核心模块，在项目中只加载一次
+import { NgModule , SkipSelf , Optional } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from  './header/header.component';
+import { FooterComponent } from  './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent
+  ],
+  //如果不导出，则只能在coreModule中可用，在其他地方是不可用的
+  exports:[
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent
+  ]
 })
-export class CoreModule { }
+export class CoreModule {
+  //@SkipSelf()防止进入死循环，当有父机模块的时候就去父级查看了，不用在这里循环
+  //@Optional() 说CoreModule这个依赖是可选的，如果父级模块不存在，就满足了，正常构造就好
+  constructor(@Optional() @SkipSelf() parent: CoreModule){
+    if (parent){
+      throw new Error("模块已经存在，不能再次加载！");
+    }
+  }
+}
